e7f299d5287ff4889cac673aa7f63583051c88d9
Sam Okpara <samokpara@Sams-MacBook-Pro.local>
2019-03-20 13:03:01 -0400

    Solve reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,27 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    // Change string to Array
+    // Reverse Array
+    // Change array back to String
+    let reversed = '';
+    
+    for(let char of str) {
+        reversed = char + reversed;
+    }
+    return reversed;
+}
+
+function reverseAlt(str) {
+    // Change string to Array
+    // Reverse Array
+    // Change array back to String
+
+    return str
+        .split('')
+        .reverse()
+        .join('');
+}
 
 module.exports = reverse;
