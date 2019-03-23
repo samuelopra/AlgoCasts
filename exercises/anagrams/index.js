a67985cc88119761f051fab2c2835d879164c80a
Sam Okpara <samokpara@Sams-MacBook-Pro.local>
2019-03-23 11:03:00 -0400

    solutions for anagrams and maxchar problems

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,35 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function createCharMap(str) {
+    const charMap = {};
+    for(let char of str) {
+        if(charMap[char]) {
+            charMap[char]++;
+        } else {
+            charMap[char] = 1
+        }
+    }
+    
+    console.log(charMap);
+    return charMap;
+}
+function anagrams(stringA, stringB) {
+    // create maps for eac h string
+    // compare sizes of maps
+    // iterate through a map and check if it is equal to other map
+    const charMapA = createCharMap(stringA);
+    const charMapB = createCharMap(stringB);
+
+    if(Object.keys(charMapA).length !== Object.keys(charMapB).length ) {
+        return false;
+    }
+    for(let char in charMapA) {
+        if(charMapB[char] !== charMapA[char] || !charMapB[char]) {
+            return false;
+        }
+    }
+    return true;
+}
 
 module.exports = anagrams;
--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,35 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+
+//Common similar questions:
+//What is the most common character in the string
+//Does String A have the same characters as String B? (Anagrams)
+//Does the given string have any repeated characters in it?
+function maxChar(str) {
+
+    //Create map object from String
+    //Keep track of max and maxChar
+    //Loop through map and set max char
+    
+    const charMap = {};
+    let max = 0;
+    let maxChar = '';
+
+    for(let char of str) {
+        charMap[char] = charMap[char] + 1 || 1;
+    }
+
+
+    for(let char in charMap) {
+        if(charMap[char] > max) {
+            max = charMap[char];
+            maxChar = char;
+        }
+    }
+
+    return maxChar;
+
+}
 
 module.exports = maxChar;
